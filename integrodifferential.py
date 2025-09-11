c9604afb37f86684bcca86b51eeddebb3473b7db
Andrea Di Primio <andrea.diprimio@mail.polimi.it>
2025-09-11 10:11:51 +0200

    [NEW] Code refactoring: working solver, keeping old code for backup.

---
+++ integrodifferential.py
@@ -0,0 +1,218 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+from dataclasses import dataclass, make_dataclass, asdict
+from abc import ABC, abstractmethod
+from collections.abc import Callable
+import matplotlib.pyplot as plt
+
+@dataclass
+class NumericalParameters:
+    """
+    Helper class to handle numerical parameters.
+    """
+    n_elements: int = 500
+    tol: float = 1e-8
+    smoothing: float = 0.5
+    max_iter: int = 300
+    solver: str = 'RK45'
+    final_time: float = 50
+    exact: Callable[[np.ndarray], np.ndarray] = None
+    mesh: np.ndarray = None
+
+    def __str__(self):
+        """
+        Prints class in format tag: value.
+        """
+        attrs = {**type(self).__dict__, **self.__dict__}
+        attrs = {
+            k: v for k, v in attrs.items()
+            if not k.startswith("__") and not callable(v)
+        }
+        return "\n".join(f"{k}: {v}" for k, v in attrs.items())
+
+@dataclass
+class Output:
+    """
+    Helper class to handle outputs.
+    """
+    solution: np.ndarray = None
+    errors: np.ndarray = None
+
+@dataclass
+class Settings:
+    """
+    Helper class to handle settings.
+    """
+    dump_solution: bool = True
+    dump_parameters: bool = True
+    dump_convergence_data: bool = False
+    dump_label: str = 'test'
+    path_prefix: str = './'
+
+class IntegroDifferentialProblem(ABC):
+    """
+    Abstract base class for a generic linear integro-differential first-order system.
+    """
+
+    @abstractmethod
+    def source(self, t: float) -> np.ndarray:
+        """
+        The function g such that y' = F(t, y) + memory(t, y) + g(t).
+        """
+        pass
+
+    @abstractmethod
+    def memoryless_operator(self, t: float, y: np.ndarray) -> np.ndarray:
+        """
+        The operator F such that y' = F(t, y) + memory(t, y) + g(t).
+        """
+        pass
+
+    @abstractmethod
+    def kernel(self, t: float, s: np.ndarray) -> np.ndarray:
+        """
+        Interaction kernel to compute memory term. Expected integration with respect to s.
+        """
+        pass
+
+    @abstractmethod
+    def compute_memory(self, y: np.ndarray) -> np.ndarray:
+        """
+        Function to compute the memory term.
+        """
+        pass
+
+    @abstractmethod
+    def similarity_metric(self, guess: np.ndarray, solution: np.ndarray) -> float:
+        """
+        Function to compute the distance between old and new guesses.
+        """
+        pass
+
+    def __dump(self):
+        """
+        Dumps results to text files.
+        """
+        if self.Settings.dump_convergence_data:
+            filePath = self.Settings.path_prefix + 'convergence_' + self.Settings.dump_label + '.txt' 
+            np.savetxt(filePath, self.Output.errors, header="Iteration errors")
+        if self.Settings.dump_parameters:
+            filePath = self.Settings.path_prefix + 'problem_parameters_' + self.Settings.dump_label + '.txt'
+            params = asdict(self.ProblemData)
+            np.savetxt(filePath, np.array([params[k] for k in params.keys()]), header=', '.join([k for k in params.keys()]))
+            filePath = self.Settings.path_prefix + 'numerical_parameters_' + self.Settings.dump_label + '.txt'
+            params = asdict(self.NumericalParameters)
+            np.savetxt(filePath, np.array([params[k] for k in params.keys()]), header=', '.join([k for k in params.keys()]))
+        if self.Settings.dump_solution:
+            filePath = self.Settings.path_prefix + 'solution_' + self.Settings.dump_label + '.txt'
+            np.savetxt(filePath, self.Output.solution, header="Solution data")
+        return
+    
+    def plot(self, save_fig: bool = False, flabel: str = 'solution_plot', show_exact: bool = False, fig_size: tuple[float, ] = (3.5, 3.5), names: list[str] = None):
+        """
+        Plots solution.
+        """
+        plt.rcParams['text.usetex'] = True
+        plt.rcParams['figure.figsize'] = fig_size[0], fig_size[1]
+        plt.rcParams["savefig.bbox"] = 'tight'
+        plt.rcParams["savefig.dpi"] = 'figure'
+        plt.rcParams["savefig.format"] = 'pdf'
+        if names is None:
+            names = [f'$y_{i+1}$' for i in range(len(self.Output.solution))]
+        if show_exact:
+            exact = self.NumericalParameters.exact(self.NumericalParameters.mesh)
+        for i in range(len(self.Output.solution)):
+            plt.plot(self.NumericalParameters.mesh, self.Output.solution[i], label='Solution plot ('+names[i]+')', color='blue')
+            if show_exact:
+                plt.plot(self.NumericalParameters.mesh, exact[i], '-.', label='Exact solution', color='red')
+                plt.title('Approximate solution vs exact solution ('+names[i]+')')
+            else:
+                plt.title('Approximate solution ('+names[i]+')')
+            plt.xlabel('$t$')
+            plt.ylabel(names[i])
+            plt.grid(True)
+            plt.legend()
+            if save_fig:
+                plt.savefig(self.Settings.path_prefix + flabel + f'_{names[i]}.pdf')
+                plt.close()
+            else:
+                plt.show()
+        return
+    
+    def __compute_right_hand_side(self, t: float, y: np.ndarray) -> np.ndarray:
+        """
+        Internal routine to estimate y' as F(t, y) + g(t).
+        """
+        return self.memoryless_operator(t, y) + self.source(t)
+    
+    def __set_memory_operator(self) -> np.ndarray:
+        """
+        Builds the complete integro-differential operator. It assumes the equation with memory is the last one.
+        """
+        def memory_operator(t: float, y: np.ndarray) -> np.ndarray:
+            T = self.NumericalParameters.final_time
+            N = self.NumericalParameters.n_elements
+            memory_interp = lambda t_val: self.memory[min(int(np.floor(np.clip(t_val, 0, T) * (N - 1) / T)), N - 1)]
+            return self.__compute_right_hand_side(t, y) + np.array([0, 0, 0, memory_interp(t)])
+        return memory_operator
+    
+    def __set_problem_data(self, initial: np.ndarray, **params):
+        """
+        Creates a data class with problem-specific parameters.
+        """
+        fields = [(k, type(v)) for k, v in params.items()] + [('initial', list[float])]
+        ProblemData = make_dataclass('ProblemData', fields)
+        params['initial'] = initial
+        self.ProblemData = ProblemData(**params)
+        return
+          
+    def __init__(self, initial, **params):
+        """
+        Forces all derived classes to build data classes for parameters.
+        """
+        self.__set_problem_data(initial, **params)
+        self.NumericalParameters = NumericalParameters()
+        self.Output = Output() 
+        self.Settings = Settings()
+        return
+    
+    def solve(self):
+        """Solve routine."""
+        self.memory = np.zeros(self.NumericalParameters.n_elements)
+        self.NumericalParameters.mesh = np.linspace(0, self.NumericalParameters.final_time, self.NumericalParameters.n_elements)
+        iter = 0
+        guess = solve_ivp(self.__compute_right_hand_side,
+                        [0, self.NumericalParameters.final_time],
+                        self.ProblemData.initial,
+                        method=self.NumericalParameters.solver,
+                        t_eval=self.NumericalParameters.mesh,
+                        rtol=1e-8,
+                        atol=1e-10).y
+        done = False
+        iteration_errors = []
+        while not done and iter < self.NumericalParameters.max_iter:
+            iter += 1
+            self.compute_memory(guess[-1])
+            memory_operator = self.__set_memory_operator()
+            vector_solution = solve_ivp(memory_operator,
+                                    [0, self.NumericalParameters.final_time],
+                                    self.ProblemData.initial,
+                                    method=self.NumericalParameters.solver,
+                                    t_eval=self.NumericalParameters.mesh,
+                                    rtol=1e-8,
+                                    atol=1e-10).y
+            current_error = self.similarity_metric(guess, vector_solution)
+            print(f'current_error = {current_error}') # temporary
+            iteration_errors.append(current_error)
+            if current_error <= self.NumericalParameters.tol:
+                done = True
+                print(f"Converged after {iter} iterations with error {current_error:.2e}")
+            else:
+                guess = self.NumericalParameters.smoothing * vector_solution + (1 - self.NumericalParameters.smoothing) * guess
+        if iter >= self.NumericalParameters.max_iter:
+            print(f"Warning: Maximum iterations ({self.NumericalParameters.max_iter}) reached without convergence.")
+            print(f"Final error: {current_error[-1]:.2e}")
+        self.Output.solution = guess
+        self.Output.errors = iteration_errors
+        self.__dump()
+        return
---
+++ main.py
@@ -0,0 +1,108 @@
+import numpy as np
+from scipy.integrate import simpson
+import matplotlib.pyplot as plt
+from integrodifferential import IntegroDifferentialProblem
+
+class MGTGPSolver(IntegroDifferentialProblem):
+
+    def __init__(self, initial, params):
+        super().__init__(initial, **params)
+
+    def source(self, t: float) -> np.ndarray:
+        """
+        The function g such that y' = F(t, y) + memory(t, y) + g(t).
+        """
+        return np.array([0, 0, \
+                     2 * (self.ProblemData.alpha + self.ProblemData.delta * self.ProblemData.mode) + 2 * self.ProblemData.beta * self.ProblemData.mode * t + self.ProblemData.gamma * self.ProblemData.mode * (t ** 2 + 0.1) - self.ProblemData.rho * self.ProblemData.mode * (0.1 * np.cos(t) + t), \
+                     # 2 * t + mode * initial[-1] * np.exp(-t) + mode * ((t ** 2 - 2 * t + 2.1) - 2.1 * np.exp(-t)) + 2 * rho * mode + 2 * rho * gamma / beta * mode * t
+                     - 0.1 * np.sin(t) + 1 + self.ProblemData.mode * self.ProblemData.initial[-1] * np.exp(-t) + self.ProblemData.mode * (0.1 * 0.5 * (np.cos(t) + np.sin(t) - np.exp(-t)) + t - 1 + np.exp(-t)) + 2 * self.ProblemData.rho * self.ProblemData.mode + 2 * self.ProblemData.rho * self.ProblemData.gamma / self.ProblemData.beta * self.ProblemData.mode * t])
+
+    def memoryless_operator(self, t: float, y: np.ndarray) -> np.ndarray:
+        """Computes F(t, y), i.e. the ODE part without memory."""
+        y2 = (- self.ProblemData.gamma * self.ProblemData.mode * y[0]
+                - self.ProblemData.beta * self.ProblemData.mode * y[1]
+                - (self.ProblemData.delta * self.ProblemData.mode + self.ProblemData.alpha) * y[2]
+                + self.ProblemData.rho * self.ProblemData.mode * y[3])
+        y3 = (- self.ProblemData.rho * self.ProblemData.mode * self.ProblemData.gamma / self.ProblemData.beta * y[1]
+                - self.ProblemData.rho * self.ProblemData.mode * y[2]
+                - self.ProblemData.mode * y[3])
+        return np.array([y[1], y[2], y2, y3])
+    
+    def kernel(self, t: float, s: np.ndarray) -> np.ndarray:
+        """
+        Interaction kernel to compute memory term. Expected integration with respect to s.
+        """
+        truncation = 40.0
+        diff = t - s
+        val = np.exp(- diff)
+        return np.where(diff <= truncation, val, 0.0)
+
+    def compute_memory(self, y: np.ndarray) -> np.ndarray:
+        """
+        Function to compute the memory term. This function directly stores values in self.memory.
+        """
+        factor2 = - self.ProblemData.mode * y
+        for i in range(self.NumericalParameters.n_elements):
+            quadMesh = self.NumericalParameters.mesh[0:i+1]
+            factor1 = self.kernel(self.NumericalParameters.mesh[i], quadMesh)
+            ynew = factor1 * factor2[0:i+1]
+            self.memory[i] = simpson(ynew, x=quadMesh) - self.ProblemData.initial[-1] * self.ProblemData.mode * np.exp(- self.NumericalParameters.mesh[i]) + self.ProblemData.mode * y[i] # to be generalized, works only for exp kernels
+        return
+
+    def similarity_metric(self, guess: np.ndarray, solution: np.ndarray) -> float:
+        """
+        Function to compute the distance between old and new guesses.
+        """
+        return np.sum((solution[-1] - guess[-1]) ** 2)
+    
+    def mu(self, t: np.ndarray) -> np.ndarray:
+        """
+        Integral of the kernel from t to infinity. 
+        """
+        truncation = 30
+        val = np.exp(-t)
+        return np.where(t <= truncation, val, np.zeros_like(t))
+    
+    def exact(self, t: np.ndarray) -> np.ndarray:
+        """
+        Computes exact solution.
+        """
+        y1 = 0.1 + t ** 2
+        y2 = 2 * t
+        y3 = 2 * np.ones(self.NumericalParameters.n_elements)
+        y4 = 0.1 * np.cos(t) + t
+        return [y1, y2, y3, y4]
+
+def main():
+    # === Instantiate solver ===
+    params = {'alpha': 1.0,
+              'beta': 100.0,
+              'gamma': 20.0,
+              'delta': 1.0,
+              'rho': 1.0,
+              'mode': 4.0}
+    initial_conditions = [0.1, 0, 2.0, 0.1]
+    solver = MGTGPSolver(initial_conditions, params)
+
+    # === Configure numerical parameters ===
+    solver.NumericalParameters.n_elements = 500
+    solver.NumericalParameters.final_time = 50
+    solver.NumericalParameters.tol = 1e-8
+    solver.NumericalParameters.max_iter = 300
+    solver.NumericalParameters.smoothing = 0.9
+    solver.NumericalParameters.solver = "Radau"
+    solver.NumericalParameters.exact = solver.exact
+
+    # === Configure settings ===
+    solver.Settings.dump_parameters = False
+    solver.Settings.dump_solution = False
+    solver.Settings.dump_convergence_data = False
+
+    # === Solve ===
+    solver.solve()
+
+    # === Plot ===
+    solver.plot(save_fig=False, show_exact=True, names=['$u$', '$u_t$', '$u_{tt}$', '$\\theta$'])
+
+if __name__ == "__main__":
+    main()
